f4d09f84c3f78b84525b6eef75b327f84378780c
snowlyg <brendenaudrina6287@gmail.com>
2024-09-05 14:51:48 +0800

    update cmakelists.txt

--- make.sh
+++ make.sh
@@ -9,6 +9,10 @@ cd src
 # fix v.2.0  fatal error: src/_kiss_fft_guts.h: No such file or directory
 sed -i 's/#include "src\/_kiss_fft_guts.h"/#include "_kiss_fft_guts.h"/' ./src/dump_features.c
 
+export GOOS=$(go env | grep GOOS | cut -d "'" -f2)
+export CC="$NDK_ROOT/$NDK_VERSION/toolchains/llvm/prebuilt/$GOOS-x86_64/bin/armv7a-linux-androideabi29-clang"
+export CXX="$NDK_ROOT/$NDK_VERSION/toolchains/llvm/prebuilt/$GOOS-x86_64/bin/armv7a-linux-androideabi29-clang++"
+
 ./autogen.sh
 mkdir build_android
 cd build_android
