5659579dafc420db171224efa03ab5608848fce5
Rodin <brendenaudrina6287@gmail.com>
2024-09-07 00:02:32 +0800

    add drawin lib

--- make.sh
+++ make.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# rm -rf src
-git clone https://github.com/xiph/rnnoise.git --depth=1 src
+sudo rm -rf ./src
+sudo git clone https://github.com/xiph/rnnoise.git --depth=1 src
 # git clone https://github.com/TeaSpeak/rnnoise-cmake --depth=1 src
-cp CMakeLists.txt ./src
-cd src
+sudo cp CMakeLists.txt ./src
+cd ./src
+
+src_path=$(pwd)
 
 # fix v.2.0  fatal error: src/_kiss_fft_guts.h: No such file or directory
-sed -i 's/#include "src\/_kiss_fft_guts.h"/#include "_kiss_fft_guts.h"/' ./src/dump_features.c
+sudo sed -e 's/#include "src\/_kiss_fft_guts.h"/#include "_kiss_fft_guts.h"/g' $src_path/src/dump_features.c >$src_path/src/dump_features.c
 
 export GOOS=$(go env | grep GOOS | cut -d "'" -f2)
 export CC="$NDK_ROOT/$NDK_VERSION/toolchains/llvm/prebuilt/$GOOS-x86_64/bin/armv7a-linux-androideabi29-clang"
 export CXX="$NDK_ROOT/$NDK_VERSION/toolchains/llvm/prebuilt/$GOOS-x86_64/bin/armv7a-linux-androideabi29-clang++"
 
-./autogen.sh
-mkdir build_android
+sudo ./autogen.sh
+sudo rm -rf build_android
+sudo mkdir build_android
 cd build_android
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
+sudo cmake -DCMAKE_BUILD_TYPE=Release ..
+sudo make
 
-cp librnnoise.a ../../lib/librnnoise-android-armv7.a
+sudo cp librnnoise.a ../../lib/librnnoise-drawin-armv7.a
--- rnnoise_static.go
+++ rnnoise_static.go
@@ -6,5 +6,6 @@ package rnnoise
 //#cgo CFLAGS: -I${SRCDIR}/include
 //#cgo CXXFLAGS: -I${SRCDIR}/include
 //#cgo android,arm LDFLAGS: ${SRCDIR}/lib/librnnoise-android-armv7.a -lm
-//#cgo linux,x64 LDFLAGS: ${SRCDIR}/lib/librnnoise-linux-x64.a -lm
+//#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/librnnoise-linux-arm64.a -lm
+//#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/lib/librnnoise-darwin-arm64.a
 import "C"
