d2d4feda941f07828e293d6a1649d25e166a40d0
Rodin <brendenaudrina6287@gmail.com>
2024-09-07 18:30:58 +0800

    add ProcessAudio

--- rnnoise.go
+++ rnnoise.go
@@ -7,89 +7,26 @@ import (
 	"unsafe"
 )
 
-// const FrameSize = 480
-
-// func Run() {
-// 	if len(os.Args) != 3 {
-// 		fmt.Fprintf(os.Stderr, "Usage: %s <input noisy file> <output denoised file>\n", os.Args[0])
-// 		os.Exit(1)
-// 	}
-
-// 	inputFile := os.Args[1]
-// 	outputFile := os.Args[2]
-
-// 	// Open input and output files
-// 	f1, err := os.Open(inputFile)
-// 	if err != nil {
-// 		log.Fatalf("Failed to open input file: %v", err)
-// 	}
-// 	defer f1.Close()
-
-// 	fout, err := os.Create(outputFile)
-// 	if err != nil {
-// 		log.Fatalf("Failed to create output file: %v", err)
-// 	}
-// 	defer fout.Close()
-
-// 	// Initialize RNNoise
-// 	st := C.rnnoise_create(nil)
-// 	defer C.rnnoise_destroy(st)
-
-// 	buf := make([]int16, FrameSize)
-// 	outBuf := make([]float32, FrameSize)
-
-// 	first := true
-
-// 	for {
-// 		// Read a frame of audio samples
-// 		err := binaryRead(f1, buf)
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		if err != nil {
-// 			log.Fatalf("Failed to read input file: %v", err)
-// 		}
-
-// 		// Convert int16 to float32
-// 		for i := range buf {
-// 			outBuf[i] = float32(buf[i])
-// 		}
-
-// 		// Denoise the frame
-// 		C.rnnoise_process_frame(st, (*C.float)(unsafe.Pointer(&outBuf[0])), (*C.float)(unsafe.Pointer(&outBuf[0])))
-// 		// Convert float32 back to int16
-// 		for i := range outBuf {
-// 			buf[i] = int16(outBuf[i])
-// 		}
-
-// 		// Write the denoised frame to the output file, skip the first frame
-// 		if !first {
-// 			err = binaryWrite(fout, buf)
-// 			if err != nil {
-// 				log.Fatalf("Failed to write output file: %v", err)
-// 			}
-// 		}
-
-// 		first = false
-// 	}
-
-// 	fmt.Println("Denoising completed successfully.")
-// }
-
-// // binaryRead reads a frame of int16 samples from the file.
-// func binaryRead(f *os.File, buf []int16) error {
-// 	return binary.Read(f, binary.LittleEndian, buf)
-// }
-
-// // binaryWrite writes a frame of int16 samples to the file.
-// func binaryWrite(f *os.File, buf []int16) error {
-// 	return binary.Write(f, binary.LittleEndian, buf)
-// }
-
-// process
-func Process(out []byte) {
-	// Initialize RNNoise
+func ProcessAudio(inputSamples, outputSamples []byte) {
+	// Create a new RNNoise state
 	st := C.rnnoise_create(nil)
+	// Destroy the RNNoise state
 	defer C.rnnoise_destroy(st)
-	C.rnnoise_process_frame(st, (*C.float)(unsafe.Pointer(&out[0])), (*C.float)(unsafe.Pointer(&out[0])))
+
+	// Apply RNNoise noise reduction to each frame
+	numFrames := len(inputSamples) / 4 // Assuming 16-bit PCM format (2 bytes per channel)
+	for i := 0; i < numFrames; i++ {
+		frameStart := i * 4
+		// frameEnd := frameStart + 4
+
+		// Convert bytes to int16 for RNNoise
+		samples := *(*[2]int16)(unsafe.Pointer(&inputSamples[frameStart]))
+
+		// Apply RNNoise
+
+		C.rnnoise_process_frame(st, (*C.float)(unsafe.Pointer(&samples[0])), (*C.float)(unsafe.Pointer(&samples[0])))
+		// Convert int16 back to bytes for output
+		// copy(outputSamples[frameStart:frameEnd], *(*[4]byte)(unsafe.Pointer(&samples)))
+	}
+
 }
